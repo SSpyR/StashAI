420b437f99eb89e712796f99fb1edd68f9f08402
K3io5u <k3io5u@github.com>
2018-10-29 02:18:59 -0400

    Changed rollstats and roll commands to be embedded

--- src/main/java/net/stashai/databases/dnd/StatRolls.java
+++ src/main/java/net/stashai/databases/dnd/StatRolls.java
@@ -1,5 +1,10 @@
 package net.stashai.databases.dnd;
 
+import sx.blah.discord.handle.obj.IMessage;
+import sx.blah.discord.util.EmbedBuilder;
+import sx.blah.discord.util.RequestBuffer;
+
+import java.awt.*;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Random;
@@ -8,7 +13,7 @@ public class StatRolls {
 
     private static int[] score = new int[6];
 
-    public static String[] scorerolls() {
+    public static String[] scoreRolls() {
         String[] scores;
 
         for (int i = 0; i < score.length; i++) {
@@ -43,5 +48,18 @@ public class StatRolls {
 
         Arrays.sort(ability, Collections.reverseOrder());
         return ability[0] + ability[1] + ability[2];
+    }
+
+    public static void scoreDisplay(String scores, String mods, IMessage message) {
+        EmbedBuilder embed = new EmbedBuilder();
+
+        embed.withAuthorName("Stash AI");
+        embed.withAuthorIcon("https://pre00.deviantart.net/3727/th/pre/f/2018/080/8/4/atomic___animated_4k__3840x2160__by_axiomdesign-dc6l17d.jpg");
+
+        embed.withColor(Color.CYAN);
+        embed.withTitle("Stat Rolls for " + message.getAuthor().getNicknameForGuild(message.getGuild()));
+        embed.withDescription("Ability Scores: " + scores + "\nModifiers: " + mods);
+
+        RequestBuffer.request(() -> message.getChannel().sendMessage(embed.build()));
     }
 }
